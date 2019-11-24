f5cb92704d459b0ef2db628a47e6396914ca3af6
Surajman <suraj.mce.dtu@gmail.com>
2019-11-24 14:07:52 +0530

    Updates

---
+++ precache-manifest.9a776f67a5fa898737e08c4e7b729ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2608569d85d49cb496e39528c9a71607",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "fba6eeaedcda3ea047c0",
+    "url": "/tabs/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "5c019e368dc968f9c08e",
+    "url": "/tabs/static/css/main.8d73007c.chunk.css"
+  },
+  {
+    "revision": "fba6eeaedcda3ea047c0",
+    "url": "/tabs/static/js/2.d2f240f8.chunk.js"
+  },
+  {
+    "revision": "5c019e368dc968f9c08e",
+    "url": "/tabs/static/js/main.d28dccf5.chunk.js"
+  },
+  {
+    "revision": "c3175fff0b8500ea7f10",
+    "url": "/tabs/static/js/runtime-main.50c80f97.js"
+  }
+]);
